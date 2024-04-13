faab84698baf49dafc2fac03685bf77183e1813d
Bonesteeler <theo.ebenhoech@gmail.com>
2024-04-12 21:44:33 -0700

    File button UI

---
+++ Scripts/UI/Components/DropdownMenuButton.gd
@@ -0,0 +1,8 @@
+class_name DropdownMenuButton
+
+extends Button
+
+signal item_selected(item: int, label: string)
+
+func add_item():
+
--- Scripts/UI/PlannerUI.gd
+++ Scripts/UI/PlannerUI.gd
@@ -1,17 +1,28 @@
 extends Node
 
+class UIContext:
+  var currentScene: String = ""
+  var recentScenes: Array[String] = []
+
 signal tile_selected(tile: Tile)
 signal save_current_scene()
 signal load_scene()
 
+var context: UIContext = UIContext.new()
 var resources: TileResources
 
-@onready var tileSelectorUI = $TileSelectorContainer/TileSelectorControl
-@onready var setSelectorUI = $LeftColumn/SetSelectorControl
+@onready var tileSelectorUI = $Left/HSplitContainer/TileSelectorContainer/TileSelectorControl
+@onready var setSelectorUI = $Left/HSplitContainer/LeftColumn/VSplitContainer/SetSelectorControl
+@onready var menuBar = $Top/MenuBar
 
 func _ready():
   tileSelectorUI.tile_selected.connect(set_selected_tile)
   setSelectorUI.set_selected.connect(set_selected_set)
+  var fileButton = menuBar.get_node(NodePath("./FileButton"))
+  fileButton.new_scene.connect(self._on_file_new)
+  fileButton.load_scene.connect(self._on_file_load)
+  fileButton.save_scene.connect(self._on_file_save)
+  fileButton.set_saves(["scene1", "scene2", "scene3"])
 
 func set_tile_resources(newResources: TileResources):
   resources = newResources
@@ -37,3 +48,17 @@ func _on_save_pressed():
 
 func _on_load_pressed():
   load_scene.emit()
+
+func _on_file_new():
+  context.currentScene = ""
+  print("New scene")
+
+func _on_file_load(sceneName: String):
+  context.currentScene = sceneName
+  print("PlannerUI got load event for " + sceneName)
+
+func _on_file_save():
+  if context.currentScene == "":
+    print("No scene to save")
+    return
+  print("PlannerUI got save event for " + context.currentScene)
